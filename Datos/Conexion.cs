16eda17fac9475ecdd9cff2d030b0f7ff2b03d94
55970509 <55970509@ALANBURGOS>
2024-10-31 00:33:02 -0300

    Agregar archivos de proyecto.

---
+++ Datos/Conexion.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+
+public class Conexion
+{
+    private string connectionString = "server=localhost;database=escrito;uid=root;pwd=almi1323;";
+    private MySqlConnection connection;
+
+    public MySqlConnection Conectarse()
+    {
+        if (connection == null)
+        {
+            connection = new MySqlConnection(connectionString);
+        }
+
+        if (connection.State == System.Data.ConnectionState.Closed)
+        {
+            connection.Open();
+        }
+
+        return connection;
+    }
+
+    public void Desconectarse()
+    {
+        if (connection != null && connection.State == System.Data.ConnectionState.Open)
+        {
+            connection.Close();
+        }
+    }
+}
